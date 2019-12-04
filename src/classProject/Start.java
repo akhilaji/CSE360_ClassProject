8bde809a429045a5f507ec8878d2b42f5386ca8e
bflore11 <54962167+bflore11@users.noreply.github.com>
2019-12-03 20:33:24 -0700

    Added more functionality
    This commit added text fields and buttons for submitting data in the text fields as well as a button for running the data. The program is still not entirely functioning. I have not been able to find a way to save data from the text boxes for use in other functions, as it gives me an error saying variables must be final.

--- src/classProject/Start.java
+++ src/classProject/Start.java
@@ -1,4 +1,4 @@
-package classProject;
+package gradeCalculator;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
@@ -22,10 +22,21 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JFileChooser;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Color;
 
 public class Start extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -48,47 +59,167 @@ public class Start extends JFrame {
 	 * Create the frame.
 	 */
 	public Start() {
+		// Value to hold low boundary
+		String lowerBoundary = "";
+		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
 		
 		JButton btnLoadData = new JButton("Load Data");
+		btnLoadData.setForeground(new Color(255, 140, 0));
+		btnLoadData.setBackground(Color.WHITE);
 		btnLoadData.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				chooseFile();
 			}
 		});
-		GridBagConstraints gbc_btnLoadData = new GridBagConstraints();
-		gbc_btnLoadData.insets = new Insets(0, 0, 5, 5);
-		gbc_btnLoadData.gridx = 2;
-		gbc_btnLoadData.gridy = 5;
-		contentPane.add(btnLoadData, gbc_btnLoadData);
+		
+		JLabel lblLowBoundary = new JLabel("Low Boundary");
+		
+		JLabel lblHighBoundary = new JLabel("High Boundary");
 		
 		JButton btnAppendData = new JButton("Append Data");
+		btnAppendData.setForeground(new Color(255, 140, 0));
 		btnAppendData.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				chooseFile();
 			}
 		});
-		GridBagConstraints gbc_btnAppendData = new GridBagConstraints();
-		gbc_btnAppendData.insets = new Insets(0, 0, 5, 0);
-		gbc_btnAppendData.gridx = 6;
-		gbc_btnAppendData.gridy = 5;
-		contentPane.add(btnAppendData, gbc_btnAppendData);
+		
+		String low;
+		textField = new JTextField();
+
+		textField.setColumns(10);
+		
+		
+		textField_1 = new JTextField();
+		textField_1.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				String low = "";
+				low = textField_1.getText();
+				System.out.println(lowerBoundary);
+			}
+		});
+		textField_1.setColumns(10);
+		
+		JLabel lblDeleteValue = new JLabel("Delete Value");
+		
+		JLabel lblAppendValue = new JLabel("Append Value");
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		
+		JButton btnRunData = new JButton("Run Data");
+		
+		JButton btnNewButton = new JButton("Submit");
+		
+		JButton btnSubmit = new JButton("Submit");
+		
+		JButton btnNewButton_1 = new JButton("Submit");
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+
+		});
+		
+		JButton btnNewButton_2 = new JButton("Submit");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(55)
+							.addComponent(lblLowBoundary, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+							.addGap(100)
+							.addComponent(lblHighBoundary))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addContainerGap()
+									.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(Alignment.LEADING, gl_contentPane.createParallelGroup(Alignment.LEADING)
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addContainerGap(34, Short.MAX_VALUE)
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+											.addComponent(btnLoadData, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)
+											.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addGap(59)
+										.addComponent(btnNewButton))))
+							.addPreferredGap(ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(btnAppendData, GroupLayout.PREFERRED_SIZE, 148, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+									.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(btnSubmit)
+									.addGap(23)))))
+					.addContainerGap(64, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addGap(59)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblDeleteValue)
+						.addComponent(btnNewButton_1))
+					.addPreferredGap(ComponentPlacement.RELATED, 115, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblAppendValue, Alignment.TRAILING)
+						.addComponent(btnNewButton_2, Alignment.TRAILING))
+					.addGap(90))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(163)
+					.addComponent(btnRunData)
+					.addContainerGap(176, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnLoadData)
+						.addComponent(btnAppendData))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblLowBoundary)
+						.addComponent(lblHighBoundary))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton_2)
+						.addComponent(btnNewButton_1))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblDeleteValue)
+						.addComponent(lblAppendValue))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton)
+						.addComponent(btnSubmit))
+					.addPreferredGap(ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
+					.addComponent(btnRunData)
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
 
 	}
 	
-	public void chooseFile() {
+	public String chooseFile() {
         final JFrame frame = new JFrame("Choose a file");
         
         final JFileChooser fc = new JFileChooser();
@@ -105,6 +236,11 @@ public class Start extends JFrame {
         }
         fileName = sb.toString();
         System.out.println(fileName);
+        return fileName;
 	}
+	
 
+	
 }
+
+
