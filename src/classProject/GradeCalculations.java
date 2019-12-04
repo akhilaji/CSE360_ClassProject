ed665bdaa78968d0e70641d2f925bc4bf2fbd206
Akhil Aji <akhilaji99@gmail.com>
2019-12-03 17:09:31 -0700

    Create GradeCalculations.java
    added method stubs

---
+++ src/classProject/GradeCalculations.java
@@ -0,0 +1,66 @@
+package classProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradeCalculations {
+
+	public GradeCalculations() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//create array of grades
+		ArrayList<Double> scoresList = new ArrayList<Double>();
+		
+		//create scanner to read keyboard inputs
+		
+
+	}
+	
+	void readFile() {
+		
+	}
+	
+
+	double getHigh() {
+		double max = 0;
+		
+		return max; 
+		
+	}
+	
+	double getLow() {
+		double min = Double.MAX_VALUE;
+		
+		return min;
+	}
+	
+	double median() {
+		double median = 0;
+		
+		return median;
+	}
+	
+	double mean() {
+		double mean = 0;
+		
+		return mean;
+	}
+	
+	double mode() {
+		double mode = 0;
+		
+		return mode;
+	}
+	
+	void checkBoundaries() {
+		
+	}
+	
+	
+	
+	
+}
