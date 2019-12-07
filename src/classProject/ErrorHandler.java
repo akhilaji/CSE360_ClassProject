8642fc668131f160e61f810febc1c667e729fc44
bflore11 <54962167+bflore11@users.noreply.github.com>
2019-12-06 23:44:39 -0700

    Added comments

--- src/classProject/ErrorHandler.java
+++ src/classProject/ErrorHandler.java
@@ -15,17 +15,23 @@ public class ErrorHandler {
 	ArrayList<String> errorMessages  = new ArrayList<String>();
 
 	/**
-	 * 
+	 * Constructor used to create object that handles error log.
 	 */
 	public ErrorHandler() {
 		// TODO Auto-generated constructor stub
 		ArrayList<String> errorMessages  = new ArrayList<String>();
 	}
 	
+	/**
+	* Adds a new error to the error log
+	*/
 	void addNewError(String newMessage) {
 		errorMessages.add(newMessage);
 	}
 	
+	/**
+	* Prints the error log
+	*/
 	void printArray() {
 		errorMessages.forEach(error -> {;
 			System.out.println(error);
